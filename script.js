1a6ac86f3e5a2c591f66c72b2fa7278df1509aee
MidoriNoKame <83963952+MidoriNoKame@users.noreply.github.com>
2025-03-18 19:35:56 +0300

    Dino

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ let isAlive = setInterval (function() {
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
 
-    if (cactusLeft <85 && cactusLeft > 0 && dinoTop >= 140) {
+    if (cactusLeft <50 && cactusLeft > 0 && dinoTop >= 140) {
         alert("GAME OVER!")
     }
 }, 10)
